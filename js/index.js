61f8ff3a721ebd0a1799750146b440dc1779cae1
web <>
2018-03-01 14:08:31 +0800

    2/1

--- js/index.js
+++ js/index.js
@@ -3,14 +3,15 @@ $(()=>{
     $(".magnifier").click(()=>{
         $(".searchInput").toggleClass("inputHidden");
     });
+    
     //首先建立查询函数
     function search(){
         let val = $("[data-search='input']").val;
         
         // ajax({
+        //     type:"get",
         //     url:"XXX",
-        //     data:val,
-        //     dataType:get
+        //     data:val
         // }).then((response)=>{
         //     //继续执行其他操作...
         // });
@@ -22,4 +23,32 @@ $(()=>{
         if(e.keyCode == 13) search();
         //还可以改进，通过键盘点击，触发查询函数，实时查询
     });
-});
+    console.log(1);
+    //通过AJAX过滤INPUT内容，提交node服务器，返回数据   1  发送成功，0 发送失败；
+    function sendEmail(){
+        console.log(2);
+        var yourName = $("#yourName").val();
+        var telephone = $("#telephoneNumber").val();
+        if(!yourName || !telephone){
+            alert("内容不能为空");
+            console.log(3);
+        }else{
+            console.log(4);
+            
+            ajax({
+                type:"get",
+                url:'http://127.0.0.1:1234/email',
+                data:"yourName="+yourName+"&telephone="+telephone,
+                dataType:"jsonp"
+            }).then((response)=>{
+                if(JSON.parse(response).code == 1){
+                    alert("感谢您的来信！");
+                }else{
+                    alert("操作失败，请稍后再试");
+                }
+            });
+        }
+    };
+    $("[data-sendEmail='btn']").click(sendEmail);
+    
+})
--- serve/app.js
+++ serve/app.js
@@ -1,13 +1,16 @@
 const http = require("http");
 const express = require("express");
-var bodyParser = require("body-parser");
+//var bodyParser = require("body-parser");
 var  nodemailer = require('nodemailer');
 var app = express();
 app.listen(1234);
+
 //app.use(bodyParser.urlencoded({extended:false}));
-app.post("/email",(req,res)=>{
-    var yourName = req.body.yourName;
-    var telephone = req.body.telephone;
+app.get("/email",(req,res)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    var yourName = req.query.yourName || "aaa";
+    var telephone = req.query.telephone || "123";
+    console.log(req.query.yourName);
     var transporter = nodemailer.createTransport({
         service: 'qq',//邮箱服务器
         auth: {
@@ -16,18 +19,28 @@ app.post("/email",(req,res)=>{
         }
     });
     var mailOptions = {
-        from: '173581251@qq.com', 
-        to:"984244155@qq.com", // list of receivers
-        subject: yourName+" : "+telephone, 
-        text: yourName+" : "+telephone , 
-        html:"<h2>nodemailer</h2>"
+        from: '173581251@qq.com',
+        to:[//发送邮箱列表
+            "984244155@qq.com"//实验账号
+            //"HR@ipartyfamily.com",
+            //"LP@ipartyfamily.com",
+            //"dazhifan@ipartyfamily.com",
+            //"jenny@ipartyfamily.com",
+            //"104564861@qq.com",
+            //"250815795@qq.com ",
+            //"2862369272@qq.com",
+            //"455810306@qq.com"
+        ],
+        subject: yourName+" : "+telephone,
+        text: "" ,
+        html:yourName+" : "+telephone
     };
     transporter.sendMail(mailOptions, function(error, info){
         if(!error){
-            res.send({code: 1});
+            res.json({code: 1});
         }else{
             console.log(error);
-            res.send({code: 0});
+            res.json({code: 0});
         }
     });
 });
